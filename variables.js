23398e7c904dbc92f580d17311f68bc460ad4d4e
georgegathu <ggeorge.gathu@gmail.com>
2023-03-10 20:58:28 +0300

    JS Variables

---
+++ variables.js
@@ -0,0 +1,21 @@
+/* JS VARIABLES
+-A variable is used to store data values.
+-A variable can contain any types of data, such as a string, a true or false boolean, an object, or a number.
+-There are two types of variables in JavaScript: 
+1.Local variable - is declared inside block or function. It is accessible within the function or block only.
+2.Global variable - is declared outside the function or declared with window object. It can be accessed from any function
+-There are some rules while declaring a JavaScript variable (also known as identifiers) which are:
+1.Name must start with a letter (a to z or A to Z), underscore( _ ), or a dollar( $ ) sign.
+2.After first letter we can use digits (0 to 9).
+3.JavaScript variables are case sensitive, for example b and B are different variables.
+4.Variable names cannot contain whitespace characters
+-When you use more than one word to name your variable we can use:
+1.Camel case e.g userName
+2.Snake case e.g user_name
+*/
+
+let age = 22; // define the variable age and assign the value
+let username = "Json"; // define the variable username and assign the value
+
+console.log(username); // output username
+console.log(age); // outputs age
