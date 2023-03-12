4363be75d17f95fa193ae8e78bdd1d806f90a98a
georgegathu <ggeorge.gathu@gmail.com>
2023-03-12 22:29:16 +0300

    Js Let

---
+++ let.js
@@ -0,0 +1,14 @@
+/*
+JS Let
+What does let mean in JS?
+-let allows you to declare variables that are limited to the scope of a block statement, or expression on which it is used, 
+unlike the var keyword, which declares a variable globally, or locally to an entire function regardless of block scope.
+-Variables defined with let can not be redeclared.
+ Redeclaring a variable inside a block will not redeclare the variable outside the block
+-Variables defined with let must be declared before use.
+-Variables defined with let have block scope.
+ This means Variables declared inside a { } block cannot be accessed from outside the block
+
+SYNTAX of let
+let variable_name; e.g let first_name;
+*/
