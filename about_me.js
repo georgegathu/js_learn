95800dddd885711ecfcab2d355a308de1324308c
georgegathu <ggeorge.gathu@gmail.com>
2023-03-15 11:53:26 +0300

    About me

---
+++ about_me.js
@@ -0,0 +1,7 @@
+// ABOUT ME:
+const i= {}
+i.am = 'An IT engineer and an apiring Developer'
+i.amCertifiedIn = ['CCNA', 'Cisco Cyber Security Essentials']
+i.usedTo= ['Deejay', 'Hack', 'Draw']
+i.workWith = ['Python', 'JS', 'Git', 'Windows & Linux Terminal', 'Shell Scripts']
+i.aspire = 'to inspire others and change my community through tech'
--- let.js
+++ let.js
@@ -1,8 +1,10 @@
 /*
 JS Let
 What does let mean in JS?
--let allows you to declare variables that are limited to the scope of a block statement, or expression on which it is used, 
-unlike the var keyword, which declares a variable globally, or locally to an entire function regardless of block scope.
+-let allows you to declare variables that are limited to the scope of a block
+ statement, or expression on which it is used, 
+ unlike the var keyword, which declares a variable globally, or locally to an
+ entire function regardless of block scope.
 -Variables defined with let can not be redeclared.
  Redeclaring a variable inside a block will not redeclare the variable outside the block
 -Variables defined with let must be declared before use.
