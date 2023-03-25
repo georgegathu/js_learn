822cc6ff4de146f5841da6e22715095b765f63b9
georgegathu <ggeorge.gathu@gmail.com>
2023-03-25 15:40:00 +0300

    Logical '||'

--- logical_operators.js
+++ logical_operators.js
@@ -1,8 +1,17 @@
 //Password validator:
 //1. Using "&&" operator
-let password = "@1234567";
+let password = "@123456";
 
 if(password.length >= 8 && password.includes("@")){
+    console.log("STRONG");
+} else if(password.length >= 5 && password.length <= 8){
+    console.log("MEDIUM");
+} else {
+    console.log("WEAK");
+}
+
+//2. Using "OR" operator
+if(password.length >= 8 || password.includes("@")){
     console.log("STRONG");
 } else if(password.length >= 5 && password.length <= 8){
     console.log("MEDIUM");
