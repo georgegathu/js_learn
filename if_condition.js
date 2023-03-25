9a9b70c41a4ca577c527ca5499cc8ad57f857b18
georgegathu <ggeorge.gathu@gmail.com>
2023-03-25 14:57:50 +0300

    Attendance checker

--- if_condition.js
+++ if_condition.js
@@ -1,5 +1,13 @@
-let score = 85;
+// let score = 85;
 
-if(score > 80){
-    console.log("Congrats! You have passed.")
+// if(score > 80){
+//     console.log("Congrats! You have passed.")
+// }
+
+
+//A program that cheecks if there is any student available, so we can have a class
+const students = ["Gathu", "Mary","Ignetious"];
+
+if(students.length > 0){ 
+    console.log("Class is on today");
 }
