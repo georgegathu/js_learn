c4b4d3e639922fb68802fd34fc0175d74745b0ec
georgegathu <ggeorge.gathu@gmail.com>
2023-03-25 15:15:36 +0300

    Traffic light colour checker

--- else_elseif.js
+++ else_elseif.js
@@ -1,8 +1,22 @@
-const loggedIn = true;
+//Else condition:
+// const loggedIn = true;
 
-if(loggedIn){
-    console.log("Welcome user!");
+// if(loggedIn){
+//     console.log("Welcome user!");
+// }
+// else{
+//     console.log("Please log in first!");
+// }
+
+//Else-if condition:
+let sign = "green"
+if(sign == "red"){
+    console.log("STOP!")
 }
-else{
-    console.log("Please log in first!");
+else if(sign === "yellow"){
+    console.log("Get READY!")
+}else if(sign == "green"){
+console.log("GO!")
+}else{
+    console.log("Please check the sign input!")
 }
