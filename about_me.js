2d361858594bbdd9290d412269adbc89e283cb36
georgegathu <ggeorge.gathu@gmail.com>
2023-03-27 13:47:04 +0300

    Updated about me

--- about_me.js
+++ about_me.js
@@ -2,7 +2,6 @@
 const i= {}
 i.am = 'An IT engineer and an aspiring Developer'
 i.amCertifiedIn = ['CCNA', 'Cisco Cyber Security Essentials']
-i.usedTo= ['Deejay', 'Hack', 'Draw']
-i.workWith = ['Python', 'JS', 'Git', 'Windows & Linux Terminal', 'Shell Sccripts']
+i.workWith = ['Python', 'JS', 'Git', 'Terminals', 'SQL', 'Shell Sccript']
 i.aspire = 'to inspire others and change my community through tech'
 
