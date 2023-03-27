623140601a3268f7be1f32233119d431f32ed1c5
georgegathu <ggeorge.gathu@gmail.com>
2023-03-27 15:11:30 +0300

    switch statements

---
+++ switch_statements.js
@@ -0,0 +1,22 @@
+let rating = 5;
+
+switch(rating){
+    case 5:
+        console.log("MUST WATCH ");
+        break;
+    case 4:
+        console.log("GOOD OPTION");
+        break;
+    case 3:
+        console.log("MAYBE TRY");
+        break;
+    case 2:
+        console.log("SKIP");
+        break;
+    case 1:
+        console.log("IGNORE");
+        break;
+
+    default:
+        console.log("IGNORE");
+}
