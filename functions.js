a4d3c829968b27535e8d78399afa6580487de215
georgegathu <ggeorge.gathu@gmail.com>
2023-04-01 22:18:11 +0300

    Function's Arguments & Parameters

--- functions.js
+++ functions.js
@@ -1,12 +1,15 @@
 /* 
     FUNCTIONS: Its a block of code that we can call anytime.
     We use this block of code to do repetitive work.
-    -HOW TO WRITE A FUNCTION-
 */
 
-// JS FUNCTION (A FUNCTION STORED IN A VARIABLE)
-let intro = function(){
-    console.log("Welcome to JS class!!")
-};
+// JS FUNCTION (Arguments & Parameters):
+function welcome(name){
+    console.log(`Welcome ${name}`);    
+}
+welcome("Gathu");
 
-intro();
+/*
+    NB: `name` - Is parameter
+        `Gathu` - Is argument 
+*/
