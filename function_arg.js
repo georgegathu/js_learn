008ebab26dad86877f13ab5fd1ae0b5a3dd634d4
georgegathu <ggeorge.gathu@gmail.com>
2023-04-15 22:15:47 +0300

    Adding Using Functions (Argument & Parameter)

---
+++ function_arg.js
@@ -0,0 +1,23 @@
+/*
+function welcome(name){
+    console.log(`Welcome ${name}`)
+}
+
+welcome("Hommie");
+*/
+// let name = "Hommie" 
+/*
+- Value that we pass to welcome (Hommie) is argument
+- `name` in welcome function is Parameter.
+- Inside our codeblock we can access the name parameter
+ */
+
+// Addition example:
+function addition(numOne, numTwo){
+    let result = numOne + numTwo;
+    console.log(result);
+}
+addition(1, 1);
+
+
+
