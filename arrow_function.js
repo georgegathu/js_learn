72330f0b6ccac8eea3b1fa00b07e9da8eabe9925
georgegathu <ggeorge.gathu@gmail.com>
2023-05-02 21:23:36 +0300

    Arrow Function

---
+++ arrow_function.js
@@ -0,0 +1,11 @@
+// ARROW FUNCTIONS
+let my_team = function(){
+    console.log("Manchester United");
+};
+my_team();
+
+//This here is an example of an Arrow Function
+let trophy = ()=> {
+    console.log("EPL");
+};
+trophy();
