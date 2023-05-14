eeb3125ea73bec0e6995052555f8d71e24b467b3
georgegathu <ggeorge.gathu@gmail.com>
2023-05-14 21:06:20 +0300

    Object Literals

---
+++ object_literals.js
@@ -0,0 +1,13 @@
+/*
+    Objects literals contains Keys and Values
+    You can mix any type of data types in Objects   
+*/
+let user = {
+    name: "Genaro Gatuso",
+    email: "gatuso@gmail.com",
+    age: 25,
+    isLoggedIn: false
+};
+
+console.log(user);
+
