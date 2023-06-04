6876a4f182a54a1a11a50817fc2fb336c0cf7a87
georgegathu <ggeorge.gathu@gmail.com>
2023-06-04 22:28:25 +0300

    Primitive vs Reference Types

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // Primitive vs Reference Types
+// Example 1 (Reference Type)
 let numOne = 15;
 let numTwo = numOne;
 numOne = 44;
 
 console.log(numOne);
 console.log(numTwo);
+
+// Example 2 (Primitive Type)
+objOne = {title: "Learning JS", rank:4}
+objTwo = objOne
+objOne.rank = 1
+
+console.log(objOne)
+console.log(objTwo)
